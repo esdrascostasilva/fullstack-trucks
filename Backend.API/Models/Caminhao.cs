8a74b9787d3547217af88ee475db058801aed97c
esdrascosta <esdras.cts@outlook.com>
2024-10-27 18:04:38 -0300

    feat: Adicionando os dataAnottantion para as propriedades das classes de modelo

--- Backend.API/Models/Caminhao.cs
+++ Backend.API/Models/Caminhao.cs
@@ -1,14 +1,25 @@
-﻿namespace Backend.API;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.API;
 
 public class Caminhao
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo Ano de Fabricação é obrigatório")]
     public int AnoFabricacao { get; set; }
+
+    [Required(ErrorMessage = "O campo Código de Chassi é obrigatório")]
     public string CodigoChassi { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
     public string Cor { get; set; }
 
     // FK
+    [Required]
     public int ModeloId { get; set; }
+
+    [Required]
     public int PlantaId { get; set; }
 
     // Relantionship
--- Backend.API/Models/Modelo.cs
+++ Backend.API/Models/Modelo.cs
@@ -1,8 +1,12 @@
-﻿namespace Backend.API;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.API;
 
 public class Modelo
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
     public string Nome { get; set; }
 
     // Relantionship
