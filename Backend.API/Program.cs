42fc827625a09af44658c44bb7fec4542990dfd0
esdrascosta <esdras.cts@outlook.com>
2024-10-27 18:08:43 -0300

    feat: Adicionando a declaracao do servico na classe program.cs

--- Backend.API/Program.cs
+++ Backend.API/Program.cs
@@ -1,11 +1,15 @@
+using Backend.API;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CaminhaoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
