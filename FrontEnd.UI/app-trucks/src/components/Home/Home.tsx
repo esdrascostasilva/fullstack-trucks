7740aa50f950e9f2ad3f55fad60b08f1a6884743
esdrascosta <esdras.cts@outlook.com>
2024-10-29 11:07:19 -0300

    feat: Criado a funcao para um novo registro de camonhoes

--- FrontEnd.UI/app-trucks/src/components/Home/Home.tsx
+++ FrontEnd.UI/app-trucks/src/components/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import styles from './Home.module.css';
+
 
 interface CaminhaoData {
   id: number,
@@ -29,17 +29,23 @@ function Home() {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
   const [editando, setEditando] = useState<CaminhaoData | null>(null);
+  const [novoRegistro, setNovoRegistro] = useState<Omit<CaminhaoData, 'id'> | null>(null);
+  const [modelos, setModelos] = useState<Modelo[]>([]);
+  const [plantas, setPlantas] = useState<Planta[]>([]);
 
   const URL_BASE = 'http://localhost:5262'
+  const URL_PATH = '/api/caminhao'
 
   useEffect(() => {
     getCaminhoes()
+    getModelos()
+    getPlantas()
   }, [])
 
   const getCaminhoes = async () => {
     try {
       setLoading(true);
-      const response = await fetch(`${URL_BASE}/api/caminhao`)
+      const response = await fetch(`${URL_BASE}${URL_PATH}`)
       const dataApi = await response.json()
 
       setCaminhoes(dataApi)
@@ -51,13 +57,48 @@ function Home() {
     }
   }
 
+  const handleNewTruck = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (!novoRegistro)
+      return
+
+    const novoCaminhao = {
+      anoFabricacao: novoRegistro.anoFabricacao,
+      codigoChassi: novoRegistro.codigoChassi,
+      cor: novoRegistro.cor,
+      modeloId: novoRegistro.modeloId,
+      plantaId: novoRegistro.plantaId
+    }
+
+    try {
+      const response = await fetch(`${URL_BASE}${URL_PATH}`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(novoCaminhao)
+      })
+
+      if (!response.ok)
+        throw new Error('Erro ao criar um novo caminhao')
+
+      await getCaminhoes()
+      setNovoRegistro(null)
+
+    } catch (error) {
+      console.error('Erro na criação:', error)
+      alert('Ocorreu um erro ao criar um novo caminhao')
+    }
+  }
+
   const handleUpdate = async (e: React.FormEvent) => {
-    e.preventDefault();
+    e.preventDefault()
     if (!editando)
       return
 
     try {
-      const response = await fetch(`${URL_BASE}/api/caminhao/${editando.id}`, {
+      const response = await fetch(`${URL_BASE}${URL_PATH}/${editando.id}`, {
         method: 'PUT',
         headers: {
           'Content-type': 'application/json'
@@ -77,6 +118,34 @@ function Home() {
     }
   }
 
+  const handleNovoRegistroChange = (campo: keyof Omit<CaminhaoData, 'id'>, valor: any) => {
+    if (!novoRegistro) return;
+
+    setNovoRegistro({
+      ...novoRegistro,
+      [campo]: valor
+    });
+  };
+
+  const getModelos = async () => {
+    try {
+      const response = await fetch(`${URL_BASE}/api/modelo`);
+      const data = await response.json();
+      setModelos(data);
+    } catch (error) {
+      console.error('Erro ao buscar modelos:', error);
+    }
+  };
+
+  const getPlantas = async () => {
+    try {
+      const response = await fetch(`${URL_BASE}/api/planta`);
+      const data = await response.json();
+      setPlantas(data);
+    } catch (error) {
+      console.error('Erro ao buscar plantas:', error);
+    }
+  };
 
   if (loading) return <div>Carregando...</div>;
   if (error) return <div>Erro: {error}</div>;
@@ -89,17 +158,106 @@ function Home() {
       <div className={styles.container}>
         <h1 className={styles.title}>Lista de Caminhões</h1>
 
-        {/* Formulário de Edição */}
-        {editando && (
+        {/* Botão para criar um novo caminhao */}
+        {!editando && !novoRegistro && (
+          <button
+            onClick={() => setNovoRegistro({
+              anoFabricacao: 0,
+              codigoChassi: '',
+              cor: '',
+              modeloId: 0,
+              plantaId: 0,
+              modelo: { id: 0, nome: '' },
+              planta: { id: 0, nome: '' }
+            })}
+            className={styles.newButton}
+          >
+            Novo Caminhão
+          </button>
+        )}
+
+        {/* Formulário (serve tanto para edição quanto para criação) */}
+        {(editando || novoRegistro) && (
           <div className={styles.editForm}>
-            <h3>Editando Caminhão ID: {editando.id}</h3>
-            <form onSubmit={handleUpdate}>
+            <h3>{editando ? `Editando Caminhão ID: ${editando.id}` : 'Novo Caminhão'}</h3>
+            <form onSubmit={editando ? handleUpdate : handleNewTruck}>
+
+              <div className={styles.formGroup}>
+                <label>Modelo:</label>
+                <select
+                  value={editando ? editando.modeloId : (novoRegistro?.modeloId || '')}
+                  onChange={(e) => {
+                    const selectedId = parseInt(e.target.value);
+                    const selectedModelo = modelos.find(m => m.id === selectedId);
+
+                    if (editando) {
+                      setEditando({
+                        ...editando,
+                        modeloId: selectedId,
+                        modelo: selectedModelo!
+                      });
+                    } else if (novoRegistro && selectedModelo) {
+                      setNovoRegistro({
+                        ...novoRegistro,
+                        modeloId: selectedId,
+                        modelo: selectedModelo
+                      });
+                    }
+                  }}
+                >
+                  <option value="">Selecione um modelo</option>
+                  {modelos.map(modelo => (
+                    <option key={modelo.id} value={modelo.id}>
+                      {modelo.nome}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className={styles.formGroup}>
+                <label>Planta:</label>
+                <select
+                  value={editando ? editando.plantaId : (novoRegistro?.plantaId || '')}
+                  onChange={(e) => {
+                    const selectedId = parseInt(e.target.value)
+                    const selectedPlanta = plantas.find(p => p.id === selectedId)
+
+                    if (editando) {
+                      setEditando({
+                        ...editando,
+                        plantaId: selectedId,
+                        planta: selectedPlanta!
+                      });
+                    } else if (novoRegistro && selectedPlanta) {
+                      setNovoRegistro({
+                        ...novoRegistro,
+                        plantaId: selectedId,
+                        planta: selectedPlanta
+                      });
+                    }
+                  }}
+                >
+                  <option value="">Selecione uma planta</option>
+                  {plantas.map(planta => (
+                    <option key={planta.id} value={planta.id}>
+                      {planta.nome}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
               <div className={styles.formGroup}>
                 <label>Chassi:</label>
                 <input
                   type="text"
-                  value={editando.codigoChassi}
-                  onChange={(e) => setEditando({ ...editando, codigoChassi: e.target.value })}
+                  value={editando ? editando.codigoChassi : (novoRegistro?.codigoChassi || '')}
+                  onChange={(e) => {
+                    if (editando) {
+                      setEditando({ ...editando, codigoChassi: e.target.value })
+                    } else {
+                      handleNovoRegistroChange('codigoChassi', e.target.value)
+                    }
+                  }}
                 />
               </div>
 
@@ -107,8 +265,14 @@ function Home() {
                 <label>Cor:</label>
                 <input
                   type="text"
-                  value={editando.cor}
-                  onChange={(e) => setEditando({ ...editando, cor: e.target.value })}
+                  value={editando ? editando.cor : (novoRegistro?.cor || '')}
+                  onChange={(e) => {
+                    if (editando) {
+                      setEditando({ ...editando, cor: e.target.value })
+                    } else {
+                      handleNovoRegistroChange('cor', e.target.value)
+                    }
+                  }}
                 />
               </div>
 
@@ -116,39 +280,37 @@ function Home() {
                 <label>Ano de Fabricação:</label>
                 <input
                   type="number"
-                  value={editando.anoFabricacao}
-                  onChange={(e) => setEditando({ ...editando, anoFabricacao: Number(e.target.value) })}
-                />
-              </div>
-
-              <div className={styles.formGroup}>
-                <label>Modelo:</label>
-                <input
-                  type="number"
-                  value={editando.modeloId}
-                  onChange={(e) => setEditando({ ...editando, modeloId: Number(e.target.value) })}
-                />
-              </div>
-
-              <div className={styles.formGroup}>
-                <label>Planta:</label>
-                <input
-                  type="number"
-                  value={editando.plantaId}
-                  onChange={(e) => setEditando({ ...editando, plantaId: Number(e.target.value) })}
+                  value={editando ? editando.anoFabricacao : (novoRegistro?.anoFabricacao || '')}
+                  onChange={(e) => {
+                    if (editando) {
+                      setEditando({ ...editando, anoFabricacao: Number(e.target.value) });
+                    } else {
+                      handleNovoRegistroChange('anoFabricacao', Number(e.target.value))
+                    }
+                  }}
                 />
               </div>
 
               <div className={styles.formButtons}>
-                <button type="submit">Salvar</button>
-                <button type="button" onClick={() => setEditando(null)}>Cancelar</button>
+                <button type="submit">
+                  {editando ? 'Salvar' : 'Criar'}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setEditando(null);
+                    setNovoRegistro(null);
+                  }}
+                >
+                  Cancelar
+                </button>
               </div>
             </form>
           </div>
         )}
 
 
-
+        {/* Get */}
         <table className={styles.table}>
           <thead className={styles.tableHeader}>
             <tr>
