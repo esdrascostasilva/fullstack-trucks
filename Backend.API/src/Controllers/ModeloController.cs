d69438444358b0d7f5dbcc2665ed384ce4c95233
esdrascosta <esdras.cts@outlook.com>
2024-10-29 10:36:46 -0300

    feat: Criado as controllers de Modelo e Planta para usar o select box no projeto do frontend

---
+++ Backend.API/src/Controllers/ModeloController.cs
@@ -0,0 +1,25 @@
+﻿using Backend.API.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ModeloController : ControllerBase
+{
+    private readonly CaminhaoDbContext _context;
+
+    public ModeloController(CaminhaoDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var modelos = await _context.Modelos.ToListAsync();
+        return Ok(modelos);
+    }
+}
---
+++ Backend.API/src/Controllers/PlantaController.cs
@@ -0,0 +1,25 @@
+﻿using Backend.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlantaController : ControllerBase
+{
+    private readonly CaminhaoDbContext _context;
+
+    public PlantaController(CaminhaoDbContext dbContext)
+    {
+        _context = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var plantas = await _context.Plantas.ToListAsync();
+
+        return Ok(plantas);
+    }
+}
